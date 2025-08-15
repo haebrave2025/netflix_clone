032aff6a278debc887a4af66d90776b5e180ac95
hanhaeun <b01055300949@gmail.com>
2025-08-15 11:19:05 +0900

    axios test#2 (tv)

--- src/pages/TvList.jsx
+++ src/pages/TvList.jsx
@@ -1,14 +1,45 @@
 import React, {useEffect, useState} from 'react';
 import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 
 const TvList = () => {
 
     const [tvs, setTvs] = useState([])
 
+
+    const getTvList = async () => {
+        console.log("티비정보 가져옴")
+        const url = 'https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=1';
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NTk3ZTQ5MWVkNmU4MGYwZGUxMmUzNDllYjYwZWE2ZSIsIm5iZiI6MTUzMzE5MjY1NS4yMDYsInN1YiI6IjViNjJhOWNmMGUwYTI2N2VlNzAyYjdkYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xv4oEQTKm9GbehUNW1O_xByTJ78x0-FFiO8_E2mts5o'
+            }
+        };
+
+
+
+        try {
+            const result = await axios.get(url, options)
+
+            console.log(result.data.results)
+            setTvs(result.data.results)
+
+        } catch (err) {
+            console.log("+++++++++++++++++++++", err.message)
+        }
+
+
+    };
+
+
+
+
     useEffect(() => {
-        setTvs(tvInfo.results)
+         getTvList()
     }, []);
 
 
